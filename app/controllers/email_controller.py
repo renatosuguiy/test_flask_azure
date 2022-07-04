9fa539fc711bd0008dab6a32ddd45e33931238ef
Renato Suguiy <renatosuguiy@gmail.com>
2022-07-04 09:33:17 -0300

    feat: Added email endpoint

---
+++ app/controllers/email_controller.py
@@ -0,0 +1,21 @@
+import json
+import os
+from flask import jsonify
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+def send_email():
+    message = Mail(
+        from_email="renato.suguiy@pucpr.edu.br",
+        to_emails="renatosuguiy@gmail.com",
+        subject="Sending with Twilio SendGrid is Fun",
+        html_content="<strong>and easy to do anywhere, even with Python</strong>",
+    )
+
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+    return jsonify({"msg": "Sent"})
--- app/routes/test_routes.py
+++ app/routes/test_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 
 from app.controllers import list_files_azure, return_name, create_container
+from app.controllers.email_controller import send_email
 
 
 bp_test = Blueprint("test", __name__, url_prefix="/test")
 bp_test.get("/<name>")(return_name)
 bp_test.get("/create_container")(create_container)
 bp_test.get("/list_files")(list_files_azure)
+bp_test.get("/send_mail")(send_email)
 # bp_test.get("/test_read_files")()
